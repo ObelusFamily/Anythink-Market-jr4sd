ee25e60db3ce11381830c3e380575b9eeedf4ec6
Kerim Mandzo <kerman07@live.com>
2022-11-15 21:06:52 +0100

    modified the seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(100) do |i|
+    User.create(username: "User#{i}", email: "user#{i}@exam.com", password: "password!")
+ end
+
+1.upto(100) do |i|
+    Item.create(title: "Item#{i}", description: "test", user_id: i)
+end
+
+1.upto(100) do |i|
+    Comment.create(body: "Comment#{i}", item_id: i, user_id: i)
+end
